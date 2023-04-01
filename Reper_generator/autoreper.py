c40b68409a40d59f2b621c99a6a7be39baeff333
h3rmann <ibanehu@gmail.com>
2023-04-01 19:13:44 +0200

    database & rules inplemented

--- Reper_generator/autoreper.py
+++ Reper_generator/autoreper.py
@@ -1,3 +1,10 @@
-# Oniño eztau hasitze
+# Que me cuadre dropeoa baten bakarrik, edo () tarte bat egotie desafine eta afinetako
+# Haizezkoak separeu turnotan deskantsuek eukitzeko (bat hasieran, bat amaieran)
+# Potente hasi, potente amaitzu eta dinamika bitzartien (bai hasteko bai amaitzeko kantak ultra kontroleta egonbidie, hobienetariko sonetan dabienak izenbidie, eta berdiñe deskantsue baño ariñau eta gerokoak)
+# Zentzu historiko apurbet kantan kokapenagaz, istori bat kontetie
+# Tonalidadien arteko antzekotasune kanta batetik bestera, eta segiduen eitzeko kasuen iñor ez egotie konpromissed
+# Bai Helen bai edonorentzat bi kanta fisikoki potente segiduen ekidin
+# Nun joten dogunan arabera aukeratu kantak eta ordena
+# Tenpoan arteko bai antzekotasun bai kontrastie
 
 print('Hello Zazpizar ez')

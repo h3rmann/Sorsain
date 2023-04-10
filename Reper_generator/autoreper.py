747e8904fe3045f86aeed70d7dcfda7dd537a2a3
h3rmann <ibanehu@gmail.com>
2023-04-10 13:02:13 +0200

    txt format change & dict function working

--- Reper_generator/autoreper.py
+++ Reper_generator/autoreper.py
@@ -5,9 +5,30 @@
 # Tonalidadien arteko antzekotasune kanta batetik bestera, eta segiduen eitzeko kasuen iñor ez egotie konpromissed
 # Bai Helen bai edonorentzat bi kanta fisikoki potente segiduen ekidin
 # Nun joten dogunan arabera aukeratu kantak eta ordena
-# Tenpoan arteko bai antzekotasun bai kontrastie
+# Generoan arteko kongruentzi bai barietatie
+# Tenpoan arteko bai antzekotasun bai kontrastie, depende lo que interese
 
-cq_maj = ['C', 'G', 'D', 'A', 'E', 'B', 'F#', 'Db', 'Ab', 'Eb', 'Bb', 'F']
-cq_min = ['Am', 'Em', 'Bm', 'F#m', 'Dbm', 'Abm', 'Ebm', 'Bbm', 'Fm', 'Cm', 'Gm', 'Dm']
+song_list = 'atarata.txt'
+
+def song_info(song_list):
+    '''
+    This function takes the list of songs the band is familiar with as an input and converts it to a dictionary 
+    where the key is the name of the song and the values are relevant information in order to decide the order
+    in which the songs will be played.
+    '''
+    song_dict = {}
+    file = open(song_list, "r")
+    for line in file:
+        words = line.split('-')
+        name = words.pop(0)
+        song_dict[name] = words
+    file.close()
+    return song_dict
+
+def key_relation():
+    cq_maj = ['C', 'G', 'D', 'A', 'E', 'B', 'F#', 'Db', 'Ab', 'Eb', 'Bb', 'F']  # Circulo de quintas mayor
+    cq_min = ['Am', 'Em', 'Bm', 'F#m', 'Dbm', 'Abm', 'Ebm', 'Bbm', 'Fm', 'Cm', 'Gm', 'Dm']  # Circulo de quintas menor
+    return True
 
 print('Hello, Sorsain')
+print(song_info(song_list))
